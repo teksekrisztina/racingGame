5749885e712f8e283cb59c786fe62aa81d7e6d20
Tekse Krisztina <tkrisztina94@yahoo.com>
2018-12-12 21:14:47 +0200

    studied classes, variables and primitive data types

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,43 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+            //declaring a variable and instantiating a Car object
+            //this is also called initializing a variable
+    {
+        Car firstCompetitor = new Car();
+        firstCompetitor.name = "Dacia";
+        firstCompetitor.color = "black";
+        firstCompetitor.doorCount = 5;
+        firstCompetitor.mileage = 9.5;
+        //local variables are variables declared inside a method
+
+
+        Engine firstEngine = new Engine();
+        firstEngine.manufacturer = "Renault";
+
+        //sout prints a message to the console
+
+        System.out.println(firstEngine.manufacturer);
+        firstCompetitor.engine = firstEngine;
+
+        firstCompetitor.engine.manufacturer = "BMW";
+
+        firstCompetitor = null;
+
+        System.out.println(firstEngine.manufacturer);
+
+
+        System.out.println(firstCompetitor.engine.manufacturer);
+
+
+
+
+
+    }
+}
